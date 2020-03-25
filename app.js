d096ec615d9174cca32e2574f143993c830be7d1
SangSang_na <mingming12369852@gmail.com>
2020-03-25 16:13:38 +0800

    00

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-'use strict';
-const result = require('dotenv').config();
-if (result.error) throw result.error
-
 const linebot = require('linebot');
-const Express = require('express');
-const BodyParser = require('body-parser');
+const express = require('express');
 
-// Line Channel info
 const bot = linebot({
-  channelId: process.env.LINE_CHANNEL_ID,
-  channelSecret: process.env.LIEN_CHANNEL_SECRET,
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
+
+const app = express();
 
 const linebotParser = bot.parser();
-const app = Express();
-// for line webhook usage
+
 app.post('/linewebhook', linebotParser);
 
-app.use(BodyParser.urlencoded({ extended: true }));
-app.use(BodyParser.json());
-
-// a http endpoint for trigger broadcast
-app.post('/broadcast', (req, res) => {
-  bot.broadcast(req.body.message).then(() => {
-    res.send('broadcast ok');
+bot.on('message', function (event) {
+  event.reply(event.message.text).then(function (data) {
+    console.log('Success', data);
   }).catch(function (error) {
-    res.send('broadcast fail');
+    console.log('Error', error);
   });
 });
 
-app.listen(3000);
-
-// echo user message
-bot.on('message', function (event) {
-  // get user message from `event.message.text`
-  // reply same message
-  var replyMsg = `${event.message.text}`;
-  event.reply(replyMsg).then(function (data) {
-    console.log('ok')
-  }).catch(function (error) {
-    console.error(error)
-  });
+app.listen(process.env.PORT || 80, function () {
+  console.log('LineBot is running.');
 });
