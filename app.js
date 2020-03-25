c321c1acbe43b445b14fa1f8bcd3e2d5b19a12e9
SangSang_na <mingming12369852@gmail.com>
2020-03-25 17:10:56 +0800

    XDD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const linebot = require('linebot');
 const express = require('express');
 
-// const id_mod = require('./student_ID');
+// const id_mod = require('model/student_ID');
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -34,23 +34,57 @@ bot.on('message', function(event) {
   // 準備要回傳的內容
   var userMessage = event.message.text;
 
-  if (userMessage.lenght != 17) {
-    var replyMsg = '請輸入正確的學號格式';
-    event.reply('範例："12345678 12345678"');
-    event.reply(replyMsg).then(function(data) {
-      // 當訊息成功回傳後的處理
-    }).catch(function(error) {
-      // 當訊息回傳失敗後的處理
-    });
-  } else {
-    var replyMsg = `Hello你剛才說的是:${event.message.text}`;
-    // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
-    event.reply(replyMsg).then(function(data) {
-      // 當訊息成功回傳後的處理
-    }).catch(function(error) {
-      // 當訊息回傳失敗後的處理
-    });
-  }
+  switch (userMessage) {
+  	case userMessage.lenght != 17:
+  		var replyMsg = '格式錯誤，請再輸入一次。';
+  		event.reply('範例："12345678 12345678"');
+  		event.reply(replyMsg).then(function(data) {
+  			// 當訊息成功回傳後的處理
+  		}).catch(function(error) {
+  			// 當訊息回傳失敗後的處理
+  		});
+
+  		break;
+  	case userMessage == '如何使用?':
+  		// var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+  		event.reply('1.請輸入你及組員的學號');
+  		event.reply('範例："12345678 12345678"');
+  		// 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+  		event.reply(replyMsg).then(function(data) {
+  			// 當訊息成功回傳後的處理
+  		}).catch(function(error) {
+  			// 當訊息回傳失敗後的處理
+  		});
+  		break;
+  	case userMessage.lenght == 17:
+  		event.reply('2.請依照題目順序上傳影片。');
+  		event.reply('ps:然後不要真的傳影片。我還沒做好');
+
+  		break;
+  	case userMessage == '版本':
+  		event.reply('版本：0.1.7');
+  		break;
+  	default:
+
+  };
+
+  // if (userMessage.lenght != 17) {
+  //   var replyMsg = '請輸入正確的學號格式';
+  //   event.reply('範例："12345678 12345678"');
+  //   event.reply(replyMsg).then(function(data) {
+  //     // 當訊息成功回傳後的處理
+  //   }).catch(function(error) {
+  //     // 當訊息回傳失敗後的處理
+  //   });
+  // } else {
+  //   var replyMsg = `Hello你剛才說的是:${event.message.text}`;
+  //   // 透過event.reply(要回傳的訊息)方法將訊息回傳給使用者
+  //   event.reply(replyMsg).then(function(data) {
+  //     // 當訊息成功回傳後的處理
+  //   }).catch(function(error) {
+  //     // 當訊息回傳失敗後的處理
+  //   });
+  // }
 
 });
 
